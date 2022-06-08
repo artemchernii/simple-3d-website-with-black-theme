67f1609779fa2d63f259afb17b6230eec6df26ec
Artem Chernii <artem.chernii@osf.digital>
2022-06-08 10:21:19 +0100

    fix: fix libs problem for heroku

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-window.onload = () => {
-  document.body.classList.add('loaded');
-  setTimeout(() => {
-    Draggable.create('.gallery', {
-      bounds: 'body',
-      inertia: true,
-    });
-    console.log('loaded', document.body.classList);
-  }, 2000);
-};
+// window.onload = () => {
+//   document.body.classList.add('loaded');
+//   setTimeout(() => {
+//     Draggable.create('.gallery', {
+//       bounds: 'body',
+//       inertia: true,
+//     });
+//     console.log('loaded', document.body.classList);
+//   }, 2000);
+// };
